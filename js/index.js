02f590fba8d9c506a0bb3ee1d1766172dd148ab9
dj04304 <dj043045@gmail.com>
2022-07-28 12:57:53 +0900

    서브 및 풀다운 스크립트 추가

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const nav = document.querySelectorAll("nav li");
+const subMn = document.querySelector(".sub_mn");
+const subMnText = document.querySelector(".sub_mn_in");
+
+console.log(nav);
+console.log(subMn);
+
+// 마우스를 올렸을 때
+for(let i = 0; i < nav.length; i++) {
+    nav[i].onmouseover = () => {
+        subMn.classList.add("on");
+        subMnText.classList.add("act");
+   }
+}
+
+// 마우스를 땠을 때
+for(let i = 0; i < nav.length; i++) {
+    subMn.onmouseleave = () => {
+        subMn.classList.remove("on");
+        subMnText.classList.remove("act");
+   }
+}
